9f92cfabcffbc4407c81cd30d8179f05a66bbf16
rashikasinghh <rashika.singhh@hotmail.com>
2019-03-31 01:42:25 +0530

    create and delete done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'toDoApp';
+inputArray = [];
+
+  toDoForm = new FormGroup({
+    inputbox: new FormControl('')
+  });
+  onClick() {
+   const a = this.toDoForm.value;
+  const inputvalue = a.inputbox;
+  this.inputArray.push(inputvalue);
+  }
+
+onDelete(i) {
+this.inputArray.splice(i, 1);
+}
 }
